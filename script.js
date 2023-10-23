1020c03c8c8d7d5aa7748624a283a3159e1d3e0a
mfalezan <matthieufalezan@gmail.com>
2023-10-23 14:05:39 +0200

    Commit test

--- script.js
+++ script.js
@@ -4,3 +4,22 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+<<<<<<< Updated upstream
+=======
+
+$(document).ready(function() {
+  $('.testMgl').magnificPopup({
+    type: 'iframe',
+    iframe: {
+      patterns: {
+        youtube: {
+          index: 'youtube.com/',
+          id: 'v=',
+          src: 'https://www.youtube.com/embed/%id%?rel=0'
+        }
+      }
+    }
+  });
+});
+
+>>>>>>> Stashed changes
